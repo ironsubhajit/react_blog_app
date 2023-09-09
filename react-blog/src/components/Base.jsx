972d130c66075af159d2b47118b27648fcf0dac2
ironsubhajit <mandalsubhajit655@gmail.com>
2023-09-10 00:16:07 +0530

    Blogs list added

--- react-blog/src/components/Base.jsx
+++ react-blog/src/components/Base.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import SiteNavbar from "./SiteNavbar";
+import { Container } from "reactstrap";
 
 const Base = ({ title = "Welcome to our website", children }) => {
   return (
     <div>
       <SiteNavbar />
-      {children}
+      <Container className="mt-4">
+        {children}
+      </Container>
     </div>
   );
 };
--- react-blog/src/pages/BlogsList.jsx
+++ react-blog/src/pages/BlogsList.jsx
@@ -1,12 +1,116 @@
 import React from "react";
-import SiteNavbar from "../components/SiteNavbar";
 import Base from "../components/Base";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  CardGroup,
+  CardImg,
+  Button,
+  Row,
+  Col,
+} from "reactstrap";
+
 
 const BlogsList = () => {
+  // Demo blogs list data
+  const cardsData = [
+    {
+      title: "Card 1",
+      subtitle: "Tech",
+      content:
+        "Content for Card 1. Content for Card 1. Content for Card 1. Content for Card 1.Content for Card 1. .Content for Card 1. ",
+    },
+    {
+      title: "Card 2",
+      subtitle: "Tech",
+      content:
+        "Content for Card 2. Content for Card 2. Content for Card 2. Content for Card 2.Content for Card 2. Content for Card 2",
+    },
+    {
+      title: "Card 3",
+      subtitle: "Tech",
+      content:
+        "Content for Card 3. Content for Card 3. Content for Card 3. Content for Card 3.Content for Card 3. Content for Card 3",
+    },
+    {
+      title: "Card 4",
+      subtitle: "Tech",
+      content:
+        "Content for Card 4. Content for Card 4. Content for Card 4. Content for Card 4.Content for Card 4. Content for Card 4",
+    },
+    {
+      title: "Card 5",
+      subtitle: "Tech",
+      content:
+        "Content for Card 5. Content for Card 5. Content for Card 5. Content for Card 5.Content for Card 5. Content for Card 5",
+    },
+    {
+      title: "Card 6",
+      subtitle: "Tech",
+      content:
+        "Content for Card 6. Content for Card 6. Content for Card 6. Content for Card 6.Content for Card 6. Content for Card 6",
+    },
+    {
+      title: "Card 7",
+      subtitle: "Tech",
+      content:
+        "Content for Card 7. Content for Card 7. Content for Card 7. Content for Card 7.Content for Card 7. Content for Card 7",
+    },
+    {
+      title: "Card 8",
+      subtitle: "Tech",
+      content:
+        "Content for Card 8. Content for Card 8. Content for Card 8. Content for Card 8.Content for Card 8. Content for Card 8",
+    },
+    // Add more cards as needed
+  ];
+
   return (
     <div>
       <Base>
-        <h3>List of all blogs</h3>
+        <Row className="d-flex flex-wrap">
+          {cardsData.map((card, index) => (
+            <Col xs="12" sm="6" md="4" key={index}>
+              <Card outline key={index} className="m-2">
+                <CardImg
+                  alt="Card image cap"
+                  src="https://c0.wallpaperflare.com/preview/728/375/731/aerial-analog-background-blog.jpg"
+                  top
+                  width="100%"
+                />
+                <CardBody>
+                  <CardTitle tag="h3">{card?.title}</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    {card?.subtitle}
+                  </CardSubtitle>
+                  <CardText
+                    style={{
+                      display: "-webkit-box",
+                      WebkitLineClamp: 2,
+                      WebkitBoxOrient: "vertical",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                    className="ellipsis"
+                  >
+                    {card?.content}
+                  </CardText>
+                  <Button
+                    style={{
+                      width: "100%",
+                    }}
+                    color="dark"
+                  >
+                    Read More
+                  </Button>
+                </CardBody>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </Base>
     </div>
   );
